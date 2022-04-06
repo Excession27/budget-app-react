19346415c09ee1ec916ef6fe6f80ff4967784ddd
Excession27 <feboobef27@gmail.com>
2022-04-06 02:09:44 +0200

    Budget app in React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Header } from './components/Header/Header';
 import { ListView } from './components/ListView/ListView';
 import { Input } from './components/Input/Input';
